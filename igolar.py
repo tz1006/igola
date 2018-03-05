1ba6154d476672442fbf0f1b6cea34754e1be18a
tz1006 <a@omg.tf>
2018-03-05 03:10:30 -0800

    Update igolar.py

--- igolar.py
+++ igolar.py
@@ -9,7 +9,7 @@ import json
 ua = 'Mozilla/5.0 (Macintosh; Intel Mac OS X 10_12_1) AppleWebKit/602.2.14 (KHTML, like Gecko) Version/10.0.1 Safari/602.2.14'
 ua_m = 'Mozilla/5.0 (iPhone; CPU iPhone OS 11_1_1 like Mac OS X) AppleWebKit/604.3.5 (KHTML, like Gecko) Version/11.0 Mobile/15B150 Safari/604.1'
 
-header = {'User-Agent':ua, 'Content-Type': 'application/json', 'Referer' = 'https://www.igola.com/flights/ZH/lax-bjs_2018-03-22*2018-03-31_1*RT*Economy_0*0'}
+header = {'User-Agent':ua, 'Content-Type': 'application/json', 'Referer': 'https://www.igola.com/flights/ZH/lax-bjs_2018-03-22*2018-03-31_1*RT*Economy_0*0'}
 s = requests.session()
 s.keep_alive = False
 
@@ -23,14 +23,14 @@ def create-session():
     return sessionid
 
 
-def search()
+def search():
 url = 'https://www.igola.com/web-gateway/api-flight-polling-data-hub/packagedPolling'
 payload = json.dumps({
-"magicEnabled":true,
+"magicEnabled":True,
 "currency":"CNY",
 "lang":"ZH",
 "filters":[],
 "sorter":{},
 "pageNumber":1,
 "pageSize":20,
-"sessionId":"a19dfda1-4e78-4569-a1c1-f32e00e1f3f0"}
+"sessionId":"a19dfda1-4e78-4569-a1c1-f32e00e1f3f0"})
