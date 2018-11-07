3fadd43b4ce60641f93dc114f34666ba98b1bbb5
tz1006 <a@omg.tf>
2018-11-08 07:00:02 +0800

    Update tools.py

--- tools.py
+++ tools.py
@@ -10,6 +10,7 @@
 
 import requests
 import time
+from datetime import datetime, timedelta
 from fake_useragent import UserAgent
 
 
@@ -82,6 +83,24 @@ def format_date(date):
         date = '-'.join([y,m,d])
         return date
 
+# 生成日期列表
+def date_list(start_date, end_date):
+    start_date = start_date.split('-')
+    start_year = start_date[0]
+    start_month = start_date[1]
+    start_day = start_date[2]
+    start_time = datetime.strptime('%s-%s-%s 09:25:00' % (start_year, start_month, start_day), "%Y-%m-%d %H:%M:%S")
+    end_date = end_date.split('-')
+    end_year = end_date[0]
+    end_month = end_date[1]
+    end_day = end_date[2]
+    end_time = datetime.strptime('%s-%s-%s 09:25:00' % (end_year, end_month, end_day), "%Y-%m-%d %H:%M:%S")
+    delta_days = (end_time.date() - start_time.date()).days
+    li = []
+    for i in range(delta_days+1):
+        date = (start_time.date() + timedelta(days=i)).strftime("%Y-%m-%d")
+        li.append(date)
+    return li
 
 
 # 获取加密时间戳
